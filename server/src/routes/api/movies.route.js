a95631d8b400fecd70cf6c6ef8fefc51d2a6eb31
Kasun Vithanage <alankasun@gmail.com>
2021-05-06 01:39:46 +0530

    add validation

--- server/src/routes/api/movies.route.js
+++ server/src/routes/api/movies.route.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/movies.controller");
-// const validator = require("express-validation");
-// const { create } = require("../validations/user.validation");
+const { validate } = require("express-validation");
+const validation = require("../../validation/movie.validation");
 // const auth = require("../middlewares/authorization");
 
-router.post("/", controller.create);
+router.post("/", validate(validation.create), controller.create);
 
 router.delete("/:id", controller.delete);
 
-router.patch("/:id", controller.update);
+router.patch("/:id", validate(validation.update), controller.update);
 
 router.get("/list", controller.list);
 
---
+++ server/src/validation/movie.validation.js
@@ -0,0 +1,28 @@
+const Joi = require("joi");
+
+module.exports = {
+  create: {
+    body: Joi.object({
+      _id: Joi.forbidden(),
+      title: Joi.string().required(),
+      description: Joi.string().optional(),
+      imageUrl: Joi.string().optional().allow(null),
+      rating: Joi.number().optional(),
+      studio: Joi.string().valid("marvel", "dc").optional(),
+      releaseDate: Joi.date().allow(null).optional(),
+      tag: Joi.string().valid("trending", "other").optional(),
+    }),
+  },
+  update: {
+    body: Joi.object({
+      _id: Joi.forbidden(),
+      title: Joi.string().optional(),
+      description: Joi.string().optional(),
+      imageUrl: Joi.string().optional().allow(null),
+      rating: Joi.number().optional(),
+      studio: Joi.string().valid("marvel", "dc").optional(),
+      releaseDate: Joi.date().allow(null).optional(),
+      tag: Joi.string().valid("trending", "other").optional(),
+    }),
+  },
+};
