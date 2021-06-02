8e2d0c0788f26a4910ba085036990e5ed9239a4d
Kasun Vithanage <alankasun@gmail.com>
2021-06-02 20:02:59 +0530

    remove auth

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -2,10 +2,8 @@
 const express = require("express");
 const router = express.Router();
 
-const authRouter = require("./auth.route");
 const moviesRouter = require("./movies.route");
-
-router.use("/auth", authRouter); // mount auth paths
+ // mount auth paths
 router.use("/movies", moviesRouter); // mount movies paths
 
 module.exports = router;
